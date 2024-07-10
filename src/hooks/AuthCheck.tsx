1ea8115cc0b9d755b0e958dc4de69b08730e287c
Vishal Prajapati <vishalprajapati@Vishals-MacBook-Air.local>
2024-07-10 15:13:17 +0530

    feat: refactor codebase

---
+++ src/hooks/AuthCheck.tsx
@@ -0,0 +1,22 @@
+import { ReactNode, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
+
+export type AuthCheckType = {
+  children: ReactNode;
+};
+const AuthCheck = ({ children }: AuthCheckType) => {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const details = localStorage.getItem("userDetails");
+    if (!details) {
+      toast.error("You must enter your details before accessing this page.");
+      navigate("/login");
+    }
+  }, [navigate]);
+
+  return <>{children}</>;
+};
+
+export default AuthCheck;
